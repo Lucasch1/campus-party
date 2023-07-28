bf83aab8075c0bca3955b72c048fddc2b8cf28f6
arturch2011 <arturch2011@gmail.com>
2023-07-28 13:45:31 -0300

    login page

---
+++ src/components/walletbutton.jsx
@@ -0,0 +1,56 @@
+import web3 from '../../instances/web3'
+import { useState, useEffect } from 'react'
+import { motion } from 'framer-motion'
+
+const WalletButton = () => {
+    const [error, setError] = useState('')
+    const [address, setAddress] = useState(null)
+    //const walletButton = document.getElementById('walletButton');
+
+    useEffect(() => {
+        const getAddress = async () => {
+            try {
+                const accounts = await web3.eth.getAccounts()
+                setAddress(accounts[0])
+                documentgetElementById('walletButton').textContent = address.substring(0, 8) + '...'
+            } catch (err) {
+                setError(err.message)
+            }
+        }
+        getAddress()
+    }, [address])
+
+    const connectWalletHandler = async () => {
+        /* verificar se a metamask esta disponivel */
+        try {
+            /* pedir o wallet connect */
+            await window.ethereum.request({ method: 'eth_requestAccounts' })
+            /* pegar a lista de contas */
+            const accounts = await web3.eth.getAccounts()
+            console.log(accounts)
+            setAddress(accounts[0])
+
+            /* criar uma copia local dos contratos */
+
+            // Atualizar o texto do botão com o endereço da carteira
+            const walletButton = document.getElementById('walletButton')
+            walletButton.textContent = address.substring(0, 8) + '...'
+        } catch (err) {
+            setError(err.message)
+        }
+    }
+
+    return (
+        <motion.button
+            onClick={connectWalletHandler}
+            whileHover={{ scale: 1.1 }}
+            whileTap={{ scale: 0.9 }}
+            id="walletButton"
+            className="border-white border-solid border hover:bg-white hover:text-black py-2 px-4 rounded-full"
+        >
+            ENTRAR COM A METAMASK
+        </motion.button>
+    )
+}
+
+export default WalletButton
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,7 +1,24 @@
-export default function Home() {
+import React from 'react'
+import { motion } from 'framer-motion'
+import WalletButton from '@/components/walletbutton'
+
+const LoginPage = () => {
     return (
-        <main>
-            <h1 className="text-4xl font-bold text-center">Midas Chest</h1>
-        </main>
+        <div className="flex items-center justify-end h-screen bg-[url(../../public/bg.png)] bg-cover bg-fixed bg-center">
+            <div className="fixed inset-0 "></div>
+
+            {/* Login Box */}
+            <motion.div
+                initial={{ x: '100%', opacity: 0 }}
+                animate={{ x: 0, opacity: 1 }}
+                transition={{ duration: 0.5 }}
+                className="bg-black/75 h-full flex flex-col items-center justify-center rounded-l-2xl p-8 shadow-xl z-10 w-1/3 text-white"
+            >
+                <h1 className="ribe text-5xl font-bold mb-10">MIDAS CHEST</h1>
+                <WalletButton />
+            </motion.div>
+        </div>
     )
 }
+
+export default LoginPage
